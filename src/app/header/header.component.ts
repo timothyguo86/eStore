23bf0667fa816615a27f519c0ccf013c2d57397e
Timothy Guo <timothyguo86@gmail.com>
2025-02-13 23:38:12 -0500

    feat: add search UI to header component with FontAwesome icons

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,14 @@
+// Third party imports
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'header',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  faSearch = faSearch;
+}
