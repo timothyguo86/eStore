0c908b29380f990bd74057baaac7409be392bc68
Timothy Guo <timothyguo86@gmail.com>
2025-02-15 16:23:31 -0500

    feat: implement side navigation with dynamic categories and add category service

--- src/app/home/components/side-navigation/side-navigation.component.ts
+++ src/app/home/components/side-navigation/side-navigation.component.ts
@@ -1,4 +1,8 @@
+// Third party import
 import { Component } from '@angular/core';
+// Local import
+import { CategoryService } from '../../services/category.service';
+import { Category } from '../../types/category.type';
 
 @Component({
   selector: 'side-navigation',
@@ -6,4 +10,20 @@ import { Component } from '@angular/core';
   templateUrl: './side-navigation.component.html',
   styleUrl: './side-navigation.component.scss',
 })
-export class SideNavigationComponent {}
+export class SideNavigationComponent {
+  categories: Category[] = [];
+  mainCategories: Category[] = [];
+  subCategories: Category[] = [];
+
+  constructor(categoryService: CategoryService) {
+    this.categories = categoryService.getAllCategories();
+    this.mainCategories = this.getCategories();
+    this.subCategories = this.getCategories(this.mainCategories[0].id);
+  }
+
+  getCategories(parentCategoryId?: number): Category[] {
+    return this.categories.filter(
+      (category) => category.parent_category_id === parentCategoryId
+    );
+  }
+}
