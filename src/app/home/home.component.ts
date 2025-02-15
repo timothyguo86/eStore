007a963630e092960ff4d2ebd31b315014de0741
Timothy Guo <timothyguo86@gmail.com>
2025-02-15 17:49:05 -0500

    feat: add products component with product display and styling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,10 +4,16 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { CatNavigationComponent } from './components/catNavigation/catNavigation.component';
 import { SideNavigationComponent } from './components/side-navigation/side-navigation.component';
+import { ProductsComponent } from './components/products/products.component';
 
 @Component({
   selector: 'home',
-  imports: [HeaderComponent, CatNavigationComponent, SideNavigationComponent],
+  imports: [
+    HeaderComponent,
+    CatNavigationComponent,
+    SideNavigationComponent,
+    ProductsComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
