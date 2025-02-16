718fa4a530954d714d6234d1a3b46d243f05baea
Timothy Guo <timothyguo86@gmail.com>
2025-02-16 17:53:24 -0500

    feat: establish MySQL connection in Express server

--- eStore-server/index.js
+++ eStore-server/index.js
@@ -1,14 +1,26 @@
 const express = require("express");
+const mysql = require("mysql2");
 const app = express();
 const PORT = 5001;
 
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "1289",
+  database: "estore",
+  port: 3306,
+  multipleStatements: true,
+});
+
 app.get("/", (req, res) => {
-  let prodData = {
-    pName: "Shirts",
-    price: 45,
-    img: "shop-1.jpg",
-  };
-  res.status(200).send(prodData);
+  // Query to get all categories
+  pool.query("select * FROM categories", (err, categories) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(categories);
+    }
+  });
 });
 
 const server = app.listen(PORT, () => {
