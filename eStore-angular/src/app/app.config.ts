be5d2ee60f9d03517d642b7785beebaf5d6740a1
Timothy Guo <timothyguo86@gmail.com>
2025-02-17 22:13:43 -0500

    feat: add HTTP client provider to application configuration

--- eStore-angular/src/app/app.config.ts
+++ eStore-angular/src/app/app.config.ts
@@ -1,8 +1,14 @@
+// Third party import
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http';
+// Local import
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+  ],
 };
