b3efa1e74da3e28b6f9fc86f509dd00e2bac5701
Timothy Guo <timothyguo86@gmail.com>
2025-02-18 10:51:34 -0500

    feat: update category navigation to dynamically load categories from service

--- eStore-angular/src/app/home/components/catNavigation/catNavigation.component.ts
+++ eStore-angular/src/app/home/components/catNavigation/catNavigation.component.ts
@@ -1,4 +1,9 @@
+// Third party import
 import { Component } from '@angular/core';
+// Local import
+import { categories } from './../../sampleData/categories.data';
+import { CategoryService } from './../../services/category.service';
+import { Category } from '../../types/category.type';
 
 @Component({
   selector: 'cat-navigation',
@@ -6,4 +11,14 @@ import { Component } from '@angular/core';
   templateUrl: './catNavigation.component.html',
   styleUrl: './catNavigation.component.scss',
 })
-export class CatNavigationComponent {}
+export class CatNavigationComponent {
+  categories: Category[] = categories;
+
+  constructor(private readonly categoryService: CategoryService) {
+    categoryService.getAllCategories().subscribe((categories) => {
+      this.categories = categories.filter(
+        (category) => category.parent_category_id === null
+      );
+    });
+  }
+}
