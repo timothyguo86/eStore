2047cf13c5d287b877d61edc2371354b6e72c944
Timothy Guo <timothyguo86@gmail.com>
2025-02-20 23:47:10 -0500

    feat: add product details component with template and styles

---
+++ eStore-angular/src/app/home/components/productDetails/productDetails.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'product-details',
+  imports: [],
+  templateUrl: './productDetails.component.html',
+  styleUrl: './productDetails.component.scss',
+})
+export class ProductDetailsComponent {}
