f76f6a90328beaae5dc77c3ab48d3091c3a40028
Timothy Guo <timothyguo86@gmail.com>
2025-02-22 15:38:57 -0500

    feat: add routing for product details and update product links in the product card

--- eStore-angular/src/app/home/components/products/products.component.ts
+++ eStore-angular/src/app/home/components/products/products.component.ts
@@ -1,13 +1,14 @@
 // First party import
 import { AsyncPipe, CurrencyPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 // Local import
-import { RatingsComponent } from '../ratings/ratings.component';
 import { ProductsStoreItem } from '../../store/products.storeItem';
+import { RatingsComponent } from '../ratings/ratings.component';
 
 @Component({
   selector: 'products',
-  imports: [CurrencyPipe, RatingsComponent, AsyncPipe],
+  imports: [CurrencyPipe, RatingsComponent, AsyncPipe, RouterLink],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss',
 })
--- eStore-angular/src/app/home/home.routes.ts
+++ eStore-angular/src/app/home/home.routes.ts
@@ -1,6 +1,7 @@
 // Third party import
 import { Routes } from '@angular/router';
 // Local import
+import { ProductDetailsComponent } from './components/productDetails/productDetails.component';
 import { ProductsGalleryComponent } from './components/productsGallery/productsGallery.component';
 import { HomeComponent } from './home.component';
 
@@ -12,6 +13,10 @@ export const HOME_ROUTES: Routes = [
       {
         path: 'products',
         component: ProductsGalleryComponent,
+      },
+      {
+        path: 'product',
+        component: ProductDetailsComponent,
       },
     ],
   },
