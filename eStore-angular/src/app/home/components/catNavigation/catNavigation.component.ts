c4b275b43a77dd8d0147b1f865ef74ae8a232437
Timothy Guo <timothyguo86@gmail.com>
2025-02-22 15:48:36 -0500

    feat: update category navigation to use routerLink and conditionally display options based on route

--- eStore-angular/src/app/home/components/catNavigation/catNavigation.component.ts
+++ eStore-angular/src/app/home/components/catNavigation/catNavigation.component.ts
@@ -1,21 +1,34 @@
 // Third party import
 import { AsyncPipe } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { filter } from 'rxjs';
 // Local import
 import { Category } from '../../interfaces/category.interface';
 import { CategoriesStoreItem } from '../../store/categoriesStoreItem';
 
 @Component({
   selector: 'cat-navigation',
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, RouterLink],
   templateUrl: './catNavigation.component.html',
   styleUrl: './catNavigation.component.scss',
 })
 export class CatNavigationComponent {
-  constructor(public categoryStore: CategoriesStoreItem) {}
-
   @Output()
   categoryClicked: EventEmitter<number> = new EventEmitter<number>();
+
+  displayOptions: boolean = true;
+
+  constructor(
+    public categoryStore: CategoriesStoreItem,
+    private readonly router: Router
+  ) {
+    router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event) => {
+        this.displayOptions = event.url === '/home/products';
+      });
+  }
 
   onCategoryClicked(category: Category): void {
     this.categoryClicked.emit(category.id);
