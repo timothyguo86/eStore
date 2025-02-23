57bdedefe5f9ea5d913866b6ca2a745611eb956e
Timothy Guo <timothyguo86@gmail.com>
2025-02-23 17:17:08 -0500

    feat: implement cart component with product listing, quantity controls, and total amount management

---
+++ eStore-angular/src/app/home/components/cart/cart.component.ts
@@ -0,0 +1,41 @@
+// Third party imports
+import { CurrencyPipe } from '@angular/common';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+// Local imports
+import { CartItem } from '../../interfaces/cart.interface';
+import { CartStoreItem } from '../../store/cartStoreItem';
+import { RatingsComponent } from '../ratings/ratings.component';
+
+@Component({
+  selector: 'app-cart',
+  imports: [CurrencyPipe, RatingsComponent, FontAwesomeModule],
+  templateUrl: './cart.component.html',
+  styleUrl: './cart.component.scss',
+})
+export class CartComponent {
+  faTrash = faTrash;
+
+  constructor(
+    public cartStoreItem: CartStoreItem,
+    private readonly router: Router
+  ) {}
+
+  navigateToHome(): void {
+    this.router.navigate(['/home/products']);
+  }
+
+  increaseQuantity(cartItem: CartItem): void {
+    this.cartStoreItem.addProduct(cartItem.product);
+  }
+
+  decreaseQuantity(cartItem: CartItem): void {
+    this.cartStoreItem.decreaseProductQuantity(cartItem);
+  }
+
+  removeItem(cartItem: CartItem): void {
+    this.cartStoreItem.removeProduct(cartItem);
+  }
+}
--- eStore-angular/src/app/home/store/cartStoreItem.ts
+++ eStore-angular/src/app/home/store/cartStoreItem.ts
@@ -40,9 +40,37 @@ export class CartStoreItem extends StoreItem<Cart> {
           amount: product.price,
         },
       ];
-    } else cartProduct.quantity += 1;
+    } else {
+      cartProduct.quantity += 1;
+      cartProduct.amount += product.price;
+    }
 
     this.cart.totalAmount += product.price;
     this.cart.totalProducts += 1;
+  }
+
+  removeProduct(cartItem: CartItem): void {
+    this.cart.products = this.cart.products.filter(
+      (product) => product.product.id !== cartItem.product.id
+    );
+
+    this.cart.totalProducts -= cartItem.quantity;
+    this.cart.totalAmount -= cartItem.amount;
+  }
+
+  decreaseProductQuantity(cartItem: CartItem): void {
+    const cartProduct = this.cart.products.find(
+      (product) => product.product.id === cartItem.product.id
+    );
+
+    if (cartProduct) {
+      if (cartProduct.quantity === 1) {
+        this.removeProduct(cartProduct);
+      } else {
+        cartProduct.quantity -= 1;
+        this.cart.totalProducts -= 1;
+        this.cart.totalAmount -= cartProduct.product.price;
+      }
+    }
   }
 }
