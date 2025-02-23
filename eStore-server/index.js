5eb17d285d67dba15602b51c94d387eedaa3060d
Timothy Guo <timothyguo86@gmail.com>
2025-02-23 18:53:47 -0500

    feat: add user signup route with email validation and password hashing

--- eStore-server/index.js
+++ eStore-server/index.js
@@ -3,11 +3,15 @@ const cors = require("cors");
 const app = express();
 const products = require("./routes/products");
 const productCategories = require("./routes/productCategories");
+const users = require("./routes/users");
 const PORT = 5001;
+const bodyParser = require("body-parser");
 
 app.use(cors());
+app.use(bodyParser.json()); // Resolves the data received in the body of the request before it passes to the relevant route
 app.use("/products", products);
 app.use("/productCategories", productCategories);
+app.use("/users", users);
 
 const server = app.listen(PORT, () => {
   console.log("Server is running on port 5001");
