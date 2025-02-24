7fbb67caa454c2a10d097b38dfe9fbc2fd333953
Timothy Guo <timothyguo86@gmail.com>
2025-02-23 23:31:49 -0500

    feat: enhance user signup form with validation messages and disable button on invalid input

--- eStore-angular/src/app/home/components/users/userSignup/userSignup.component.ts
+++ eStore-angular/src/app/home/components/users/userSignup/userSignup.component.ts
@@ -1,6 +1,12 @@
 // Third party import
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormGroup,
+  FormBuilder,
+  ReactiveFormsModule,
+  Validators,
+  AbstractControl,
+} from '@angular/forms';
 // Local import
 
 @Component({
@@ -16,16 +22,32 @@ export class UserSignupComponent implements OnInit {
 
   ngOnInit(): void {
     this.userSignupForm = this.fb.group({
-      firstName: [''],
+      firstName: ['', Validators.required],
       lastName: [''],
       address: [''],
       city: [''],
       state: [''],
       pin: [''],
-      email: [''],
-      password: [''],
-      confirmPassword: [''],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
     });
+  }
+
+  get firstName(): AbstractControl<any, any> | null {
+    return this.userSignupForm.get('firstName');
+  }
+
+  get email(): AbstractControl<any, any> | null {
+    return this.userSignupForm.get('email');
+  }
+
+  get password(): AbstractControl<any, any> | null {
+    return this.userSignupForm.get('password');
+  }
+
+  get confirmPassword(): AbstractControl<any, any> | null {
+    return this.userSignupForm.get('confirmPassword');
   }
 
   onSubmit() {}
