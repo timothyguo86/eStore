280690810ed425075520ddef34fdeb169d7b7610
Timothy Guo <timothyguo86@gmail.com>
2025-02-27 01:51:47 -0500

    feat: implement user authentication display in header with dropdown menu

--- eStore-angular/src/app/home/components/header/header.component.ts
+++ eStore-angular/src/app/home/components/header/header.component.ts
@@ -1,6 +1,6 @@
 // Third party imports
 import { AsyncPipe } from '@angular/common';
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, Output } from '@angular/core';
 import { NavigationEnd, Router, RouterLink } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {
@@ -8,9 +8,10 @@ import {
   faShoppingCart,
   faUserCircle,
 } from '@fortawesome/free-solid-svg-icons';
-import { filter } from 'rxjs';
+import { filter, Subscription } from 'rxjs';
 // Local imports
 import { SearchKeyword } from '../../interfaces/searchKeyword.interface';
+import { UserService } from '../../services/userService.service';
 import { CartStoreItem } from '../../store/cartStoreItem';
 import { CategoriesStoreItem } from '../../store/categoriesStoreItem';
 
@@ -20,20 +21,23 @@ import { CategoriesStoreItem } from '../../store/categoriesStoreItem';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
-  faSearch = faSearch;
-  faUserCircle = faUserCircle;
-  faShoppingCart = faShoppingCart;
-
+export class HeaderComponent implements OnDestroy {
   @Output()
   searchClicked: EventEmitter<SearchKeyword> =
     new EventEmitter<SearchKeyword>();
 
+  faSearch = faSearch;
+  faUserCircle = faUserCircle;
+  faShoppingCart = faShoppingCart;
   displaySearch: boolean = true;
+  isUserAuthenticated: boolean = false;
+  userName: string = '';
+  subscription: Subscription = new Subscription();
 
   constructor(
     public categoryStore: CategoriesStoreItem,
     public cartStoreItem: CartStoreItem,
+    public userService: UserService,
     private readonly router: Router
   ) {
     router.events
@@ -41,6 +45,25 @@ export class HeaderComponent {
       .subscribe((event) => {
         this.displaySearch = event.url === '/home/products';
       });
+
+    this.subscription.add(
+      this.userService.isUserAuthenticated$.subscribe(
+        (isUserAuthenticated: boolean) => {
+          this.isUserAuthenticated = isUserAuthenticated;
+        }
+      )
+    );
+
+    this.subscription.add(
+      this.userService.loggedInUser$.subscribe((loggedInUser) => {
+        console.log('header component: loggedInUser', loggedInUser);
+        this.userName = loggedInUser.firstName;
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   onClickSearch(keyword: string, categoryId: string) {
