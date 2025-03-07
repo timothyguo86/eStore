78418498c4a6029bb8f67dcfe8f1e218d21eb071
Timothy Guo <timothyguo86@gmail.com>
2025-03-06 23:55:16 -0500

    feat: Add reactive form and user data binding to cart component
    Introduced a reactive form to the cart component for capturing user details such as name, address, city, state, and pin. Integrated user data binding by subscribing to the logged-in user stream from `UserService`. Added lifecycle hooks (`OnInit` and `OnDestroy`) to manage form initialization and subscriptions effectively.

--- eStore-angular/src/app/home/components/cart/cart.component.ts
+++ eStore-angular/src/app/home/components/cart/cart.component.ts
@@ -1,13 +1,17 @@
 // Third party imports
 import { CurrencyPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faTrash, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
 // Local imports
 import { CartItem } from '../../interfaces/cart.interface';
 import { CartStoreItem } from '../../store/cartStoreItem';
 import { RatingsComponent } from '../ratings/ratings.component';
+import { LoggedInUser } from '../../interfaces/user.interface';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-cart',
@@ -15,13 +19,52 @@ import { RatingsComponent } from '../ratings/ratings.component';
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss',
 })
-export class CartComponent {
-  faTrash = faTrash;
+export class CartComponent implements OnInit, OnDestroy {
+  faTrash: IconDefinition = faTrash;
+  orderForm: FormGroup = new FormGroup({});
+  user: LoggedInUser;
+  subscription: Subscription = new Subscription();
 
   constructor(
     public cartStoreItem: CartStoreItem,
     private readonly router: Router,
-  ) {}
+    private readonly fb: FormBuilder,
+    private readonly userService: UserService,
+  ) {
+    this.user = {
+      firstName: '',
+      lastName: '',
+      address: '',
+      city: '',
+      state: '',
+      pin: '',
+    };
+
+    this.subscription.add(
+      userService.loggedInUser$.subscribe((loggedInUser: LoggedInUser) => {
+        if (loggedInUser.firstName) this.user = loggedInUser;
+      }),
+    );
+  }
+
+  ngOnInit(): void {
+    this.orderForm = this.fb.group({
+      name: [
+        `${this.user.firstName} ${this.user.lastName}`,
+        Validators.required,
+      ],
+      address: [this.user.address, Validators.required],
+      city: [this.user.city, Validators.required],
+      state: [this.user.state, Validators.required],
+      pin: [this.user.pin, Validators.required],
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  onSubmit(): void {}
 
   navigateToHome(): void {
     this.router.navigate(['/home/products']);
